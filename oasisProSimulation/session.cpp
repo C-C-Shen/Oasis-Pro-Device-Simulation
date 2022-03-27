4e530776d2a5ddcda9d1a78e4b5a35b4efbfe81d
C-C-Shen <71462647+C-C-Shen@users.noreply.github.com>
2022-03-27 19:00:31 -0400

    Add intensity to session class
    This will be needed eventually when saving preferences is implemented

--- oasisProSimulation/session.cpp
+++ oasisProSimulation/session.cpp
@@ -1,9 +1,10 @@
 #include "session.h"
 
-Session::Session(QString sType, QString freq)
+Session::Session(QString sType, QString freq, int intense)
 {
     type = sType;
     frequency = freq;
+    intensity = intense;
 }
 
 Session::~Session(){}
@@ -16,4 +17,18 @@ QString Session::getSessionType()
 QString Session::getFrequency()
 {
     return frequency;
+}
+
+int Session::getIntensity()
+{
+    return intensity
+}
+
+void Session::setIntensity(int intense)
+{
+    if (intense >= 0 && intense <= 7) {
+        intensity = intense;
+    } else {
+        std::cout << "Invalid intensity setting" << std::endl;
+    }
 }
--- oasisProSimulation/session.h
+++ oasisProSimulation/session.h
@@ -6,15 +6,18 @@
 class Session
 {
     public:
-        Session(QString sType, QString freq);
+        Session(QString sType, QString freq, int intense);
         ~Session();
 
         QString getSessionType();
         QString getFrequency();
+        int getIntensity();
+        void setIntensity(int intense);
 
     private:
         QString type; // session type (ex. Delta)
         QString frequency; // frequency level (ex. 0.5 Hz)
+        int intensity; // intensity level
 };
 
 #endif // SESSION_H
